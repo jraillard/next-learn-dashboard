470a1a53b6758ceaca5f5298b5f4b96dbd527735
jraillard <ju.raillard@hotmail.fr>
2025-02-22 20:17:59 +0100

    step 14 - accessibility & formulaires
    Next inclus, par défaut "eslint-plugin-jsx-a11y" (à ajouter avec react) qui check les accessibility issues au plus tot (comme debugger chrome)
    
    useActionState => server side validation plus simple côté RSC (renvoi un objet state avec result & messagess / errors)  [SERVER SIDE]
    useFormState == useActionState mais pour ReactDom [SERVER SIDE]
    useForm => [CLIENT SIDE]
    
    Bonne compromis pour front next & backend dotnet : react hook form avec validation front + useactionstate pour récup de possibles erreurs de validations supplémentaires côté dotnet
    
    ```ts
    // React Hook Form setup (+ zod resolver bien sur)
      const { register, handleSubmit, formState: { errors } } = useForm();
    
      // Server Action setup
      const initialState: State = { message: null, errors: {} };
      const [state, formAction] = useActionState(createInvoice, initialState);
    
      // Combined submit handler
      const onSubmit = async (data: FormData) => {
        // Call server action
        formAction(data);
      };
    
      /* form shadcn ou whatever ... */
    ```
    
    ==> Dans ce contexte, penser a bloquer l'utilisateur avec un loader par dessus la page indiquant qu'on fait un appel back

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,4 +1,6 @@
-import { createInvoice } from "@/app/lib/actions";
+"use client";
+
+import { createInvoice, State } from "@/app/lib/actions";
 import { CustomerField } from "@/app/lib/definitions";
 import { Button } from "@/app/ui/button";
 import {
@@ -8,15 +10,18 @@ import {
   UserCircleIcon,
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
+import { useActionState } from "react";
 
 interface InvoiceCreateFormProps {
   customers: CustomerField[];
 }
 
 export default function Form({ customers }: Readonly<InvoiceCreateFormProps>) {
-  /* form action => permet de faire un appel API à la server action createInvoice*/
+  const initialState: State = { message: null, errors: {} };
+  const [state, formAction] = useActionState(createInvoice, initialState);
+
   return (
-    <form action={createInvoice}>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -29,6 +34,8 @@ export default function Form({ customers }: Readonly<InvoiceCreateFormProps>) {
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue=""
+              // TIPS ACCESSIBILITE : lien avec la div qui mets les erreurs
+              aria-describedby="customer-error"
             >
               <option value="" disabled>
                 Select a customer
@@ -40,6 +47,18 @@ export default function Form({ customers }: Readonly<InvoiceCreateFormProps>) {
               ))}
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+          </div>
+
+          <div
+            id="customer-error"
+            aria-live="polite"
+            /* TIPS ACCESSIBILITE : permet de notifier le screen reader quand y'a une erreur */ aria-atomic="true"
+          >
+            {state.errors?.customerId?.map((error: string) => (
+              <p className="mt-2 text-sm text-red-500" key={error}>
+                {error}
+              </p>
+            ))}
           </div>
         </div>
 
@@ -57,8 +76,20 @@ export default function Form({ customers }: Readonly<InvoiceCreateFormProps>) {
                 step="0.01"
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby="amount-error"
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            </div>
+            <div
+              id="amount-error"
+              aria-live="polite"
+              /* TIPS ACCESSIBILITE : permet de notifier le screen reader quand y'a une erreur */ aria-atomic="true"
+            >
+              {state.errors?.amount?.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
             </div>
           </div>
         </div>
@@ -77,6 +108,7 @@ export default function Form({ customers }: Readonly<InvoiceCreateFormProps>) {
                   type="radio"
                   value="pending"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  aria-describedby="status-error"
                 />
                 <label
                   htmlFor="pending"
@@ -102,7 +134,29 @@ export default function Form({ customers }: Readonly<InvoiceCreateFormProps>) {
               </div>
             </div>
           </div>
+          <div
+            id="status-error"
+            aria-live="polite"
+            /* TIPS ACCESSIBILITE : permet de notifier le screen reader quand y'a une erreur */ aria-atomic="true"
+          >
+            {state.errors?.status?.map((error: string) => (
+              <p className="mt-2 text-sm text-red-500" key={error}>
+                {error}
+              </p>
+            ))}
+          </div>
         </fieldset>
+        <div
+          id="any-error"
+          aria-live="polite"
+          /* TIPS ACCESSIBILITE : permet de notifier le screen reader quand y'a une erreur */ aria-atomic="true"
+        >
+          {state.message && (
+            <p className="mt-2 text-sm text-red-500" key="form-main-message">
+              {state.message}
+            </p>
+          )}
+        </div>
       </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
