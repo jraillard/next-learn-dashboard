017acd3e618cc97071d1be67e1dca106cb681d99
jraillard <ju.raillard@hotmail.fr>
2025-02-22 17:04:26 +0100

    step 8 - static & dynamic rendering
    NOTE : l'appli utilise principelement du dynamic rendering (c plus performant mais pas adaptable partout ..)
    
    Static rendering => Bien quand pas de data ou data partagé pour tous les users (exemple titre ou liste de produits tendance) ; pas adapté pour un dashboard dynamique
    Dynamic rendering => contenu rendu côté server pour chaque user au moment de la requête (ex : cookies & url search params)
      ==> Attention au watterfall pattern = l'agencement des requetes pour avoir un pattern d'exécution des requetes qui : fait que notre page arrive le plus rapidement possible, avec les bonnes données, en evitant le "pop effect"
      ==> Dans ce mode de rendu l'application est aussi rapide que le data fetch le plus lent

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -16,12 +16,11 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
+    // Ceci simule une latence et surtout l'importance d'avoir des Suspense (RSC) ou state isLoading (CSR) sur les traitements async
     // console.log('Fetching revenue data...');
     // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
-
-    // console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
