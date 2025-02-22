1ba8e87d1c42b907dc64db09b50e1af6081dda6e
jraillard <ju.raillard@hotmail.fr>
2025-02-22 15:57:01 +0100

    step 4 layouts

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,19 @@
+import SideNav from "@/app/ui/dashboard/sidenav";
+
+export default function Layout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+        {/** Le children est soit une page soit un layout d'une nested page
+         * Avantage des layouts = seule la page va s'update pas le layout
+         */}
+        {children}
+      </div>
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
-import { inter } from '@/app/ui/fonts';
-import '@/app/ui/global.css';
- 
+import { inter } from "@/app/ui/fonts";
+import "@/app/ui/global.css";
+
+/**
+ * Root layout for the entire application
+ * A tout intérêt à être static pour éviter les re-renders inutiles
+ * Principale utilitée : injecter des styles globaux, des providers, html / body tags et autres metadatas / analytics
+ */
 export default function RootLayout({
   children,
 }: Readonly<{
